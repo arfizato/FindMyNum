e21a5dbc6c40a13d28ed81065b79008153cee1a2
Arfizato <33662900+arfizato@users.noreply.github.com>
2021-05-07 21:46:17 +0100

    Made the design better

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from random import *
 import random as rand
 
 def outOfRange():
-    global stopPlaying
-    box = messagebox.askquestion("Error! Out Of Range!","Do You Want To Stop Playing ?",icon="warning")
-    if box=="yes":
-        stopPlaying=TRUE
+    global keepPlaying
+    box = messagebox.askquestion("Error! Out Of Range!","Do You Want To Keep Playing ?",icon="warning")
+    if box!="yes":
+        keepPlaying=FALSE
     root.destroy()
 
 def goHigher():
@@ -31,10 +31,10 @@ def goLower():
     compTxt.set(str(guess))
 
 def justRight():
-    global stopPlaying, guess
-    box = messagebox.askquestion("Your Number Is "+str(guess),"Do You Want To Stop Playing ?",icon="question")
-    if box=="yes":
-        stopPlaying=TRUE
+    global keepPlaying, guess
+    box = messagebox.askquestion("Your Number Is "+str(guess),"Do You Want To Keep Playing ?",icon="question")
+    if box!="yes":
+        keepPlaying=FALSE
     root.destroy()
 
 def ClickedOnce(guess):
@@ -42,12 +42,14 @@ def ClickedOnce(guess):
     tooLow["state"]=NORMAL
     tooHigh["state"]=NORMAL
     right["state"]=NORMAL
+    computer["state"]=DISABLED
 
-stopPlaying= FALSE
-while stopPlaying== FALSE:    
+keepPlaying= TRUE
+while keepPlaying== TRUE:    
     root= tk.Tk()
     root.title("flippity flip")
     root.iconbitmap(r'images/camera.ico')
+    root.configure(bg="#FAF9FC")
     CompGenFont= font.Font(family="Times",size=20,weight="bold",slant="italic")
     TextFont= font.Font(family="Times",weight="bold")
 
@@ -58,25 +60,25 @@ while stopPlaying== FALSE:
 
     #defining the buttons
     compTxt = tk.StringVar()
-    computer= Button(root,textvariable=compTxt,width=28,height=5,fg="#000",bg="#808080",activebackground="#808080",command= lambda : ClickedOnce(guess)  )
-    computer.grid(row=1,column=0,columnspan=3)
+    computer= Button(root, bd=0, textvariable=compTxt, width=28, height=5, fg="#978DC1", disabledforeground="#978DC1", activeforeground ='#978DC1', bg="#DCD3FE", activebackground="#ECD3FD", command= lambda : ClickedOnce(guess)  )
+    computer.grid(pady=2, row=1,column=0,columnspan=3)
     #computer["state"]=DISABLED
     computer["relief"]="sunken"
     compTxt.set("Choose a number in 0-1000 \nClick Me To Generate \nThe First Number")
     computer["font"]=CompGenFont
 
-    tooLow= Button(root,width=15,height=5,text="Too Low!",bg="#004080",activebackground="#0057ae",command= goHigher)
-    tooLow.grid(row=2,column=0)
+    tooLow= Button(root,bd=0,width=15,height=5,text="Too Low!",disabledforeground="#387DC1",activeforeground="#387DC1", fg="#387DC1",bg="#DCD3FE",activebackground="#AEE8FE",command= goHigher)
+    tooLow.grid(padx=2,pady=2,row=2,column=0)
     tooLow["font"]=TextFont
     tooLow["state"]=DISABLED
 
-    right= Button(root,width=18,height=5,text="Correct!",bg="#29411b",activebackground="#007500",command= justRight)
-    right.grid(row=2,column=1)
+    right= Button(root,bd=0,width=18,height=5,text="Correct!",disabledforeground="#5FB58E",activeforeground="#5FB58E",  fg="#5FB58E",bg="#DCD3FE",activebackground="#D9FED3",command= justRight)
+    right.grid(padx=2,pady=2,row=2,column=1)
     right["font"]=TextFont
     right["state"]=DISABLED
 
-    tooHigh= Button(root,width=15,height=5,text="Too High!",bg="#800000",activebackground="#d70000",command= goLower)
-    tooHigh.grid(row=2,column=2)
+    tooHigh= Button(root,bd=0,width=15,height=5,text="Too High!",disabledforeground="#B55A5A",activeforeground="#B55A5A", fg="#B55A5A",bg="#DCD3FE",activebackground="#FEC9DD",command= goLower)
+    tooHigh.grid(padx=2,pady=2,row=2,column=2)
     tooHigh["font"]=TextFont
     tooHigh["state"]=DISABLED
 
